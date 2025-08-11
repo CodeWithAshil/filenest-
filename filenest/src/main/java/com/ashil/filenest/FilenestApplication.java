58cfd977b0f6a593e79e27ed653c23926c005248
CodeWithAshil <ashil.kp.dev@gmail.com>
2025-08-12 00:19:12 +0530

    Initial commit

---
+++ filenest/src/main/java/com/ashil/filenest/FilenestApplication.java
@@ -0,0 +1,13 @@
+package com.ashil.filenest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilenestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilenestApplication.class, args);
+	}
+
+}
---
+++ filenest/src/test/java/com/ashil/filenest/FilenestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashil.filenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilenestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
